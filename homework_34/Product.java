f692258dd34b59f6494e4ac37faf3a5f8bb509cd
Danylo Dorofieiev <dorofieiev.dd@gmail.com>
2022-01-01 21:37:09 +0200

    HomeWork_34 was added

---
+++ homework_34/Product.java
@@ -0,0 +1,46 @@
+package homework_34;
+
+public class Product {
+    private static Long count = 0L;
+    private final Long id;
+    private String name;
+    private String category;
+    private Double price;
+
+    public Product(String name, String category, Double price) {
+        id = count++;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
